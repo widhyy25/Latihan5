cf27c9d0bf923b8f9896cc3f01eee1326c28a723
widhy25 <widiikhoerunnisa25@gmail.com>
2020-09-10 10:03:36 +0700

    Initial commit

---
+++ app/src/main/java/latihan5/example/com/latihan5/MainActivity.java
@@ -0,0 +1,33 @@
+package latihan5.example.com.latihan5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] hewan = {"kuda", "sapi", "kambing", "domba", "kerbau", "semut", "ikan", "ayam", "bebek", "jerapah", "harimau", "burung", "ular"};
+    ListView lvHewan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvHewan = (ListView) findViewById(R.id.lv_hewan);
+        ArrayAdapter<String> hewanAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, hewan);
+        lvHewan.setAdapter(hewanAdapter);
+        lvHewan.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String pilihan;
+                pilihan = (String) parent.getAdapter().getItem(position).toString();
+                Toast.makeText(getApplicationContext(), pilihan, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
